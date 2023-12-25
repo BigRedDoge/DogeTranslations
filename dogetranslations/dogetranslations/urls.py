1cb9463b43cd33ce382074fb513431bc2a28b187
Sean Clifford <seancliff01@gmail.com>
2023-12-25 10:47:59 -0500

    path for users and translations

--- dogetranslations/dogetranslations/urls.py
+++ dogetranslations/dogetranslations/urls.py
@@ -16,8 +16,13 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from django.conf import settings
+from django.conf.urls.static import static
+
 
 urlpatterns = [
+    path("__reload__/", include("django_browser_reload.urls")),
     path("admin/", admin.site.urls),
-    path('', include('translations.urls'))
-]
+    path("", include("users.urls")),
+    path("", include('translations.urls'))
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
